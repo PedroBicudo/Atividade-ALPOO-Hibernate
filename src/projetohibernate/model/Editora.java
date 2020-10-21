f5386fc7676f9e6a6d53afaf02d7189c07ae2854
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-10-21 16:18:19 -0300

    Editora adicionada.

---
+++ src/projetohibernate/model/Editora.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetohibernate.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author pedroh
+ */
+@Entity
+@Table(name="EDITORA")
+public class Editora {
+    
+    @Id
+    @Column(name = "ID_EDITORA", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @Column(name = "NOME", length = 100, nullable = false)
+    private String nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
