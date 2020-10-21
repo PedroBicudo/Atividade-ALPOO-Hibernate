870559ce4edcf65c14084114d108f92b475a947d
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2020-10-21 16:18:37 -0300

    Email adicionado.

---
+++ src/projetohibernate/model/Email.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetohibernate.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author pedroh
+ */
+@Entity
+@Table(name = "EMAIL")
+public class Email {
+    
+    @Id
+    @Column(name = "ID_EMAIL", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @Column(name = "EMAIL", nullable = false, length = 250)
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+}
